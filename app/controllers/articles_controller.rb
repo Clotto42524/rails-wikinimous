861505105b59e27cfdd68e032372c23928625e31
Clyde Choi <clydechoi@gmail.com>
2023-02-21 16:08:12 +0100

    some styling done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
   def index
+    @articles = Article.all
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
@@ -15,8 +21,22 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
